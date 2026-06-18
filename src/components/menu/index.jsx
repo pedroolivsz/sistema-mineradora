1d56ad5c27d177617f4d8ae11c492be79c6cc64d
Pedro <joaopedropereira2209@gmail.com>
2026-06-18 19:04:55 -0300

    feat: implement sidebar navigation menu

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,14 +1,65 @@
 import { NavLink } from "react-router-dom"
+
+import {
+    FaHome,
+    FaCity,
+    FaUsers,
+    FaTools,
+    FaClipboardList
+} from "react-icons/fa"
+
 import "./style.css"
 
 function Menu() {
+    const links = [
+        {
+            path:"/",
+            label: "Home",
+            icon: <FaHome />
+        },
+        {
+            path:"/cidades",
+            label: "Cidades",
+            icon: <FaCity />
+        },
+        {
+            path:"/funcionarios",
+            label: "Funcionarios",
+            icon: <FaUsers />
+        },
+        {
+            path:"/equipamentos",
+            label: "Equipamentos",
+            icon: <FaTools />
+        },
+        {
+            path:"/servicos",
+            label: "Serviços",
+            icon: <FaClipboardList />
+        }
+    ]
     return (
         <aside className="menu">
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/cidades"}>Cidades</NavLink>
-            <NavLink to={"/funcionarios"}>Funcionarios</NavLink>
-            <NavLink to={"/equipamentos"}>Equipamentos</NavLink>
-            <NavLink to={"/servicos"}>Serviços</NavLink>
+            <div className="menu-logo">
+                <h2>Mineração</h2>
+            </div>
+
+            <nav className="menu-nav">
+                {links.map((link) => (
+                    <NavLink
+                        key={link.path}
+                        to={link.path}
+                        className={({ isActive }) => 
+                            isActive
+                                ? "menu-link active"
+                                : "menu-link"
+                        }
+                    >
+                        <span>{link.icon}</span>
+                        <span>{link.label}</span>
+                    </NavLink>
+                ))}
+            </nav>
         </aside>
     )
 }
