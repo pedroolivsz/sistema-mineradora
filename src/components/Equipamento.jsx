a8fc457b5628821b24db97d61891555ed81838d6
Pedro <joaopedropereira2209@gmail.com>
2026-06-18 14:58:01 -0300

    feat: add component equipamento

---
+++ src/components/Equipamento.jsx
@@ -0,0 +1,13 @@
+import "./Equipamento.css";
+
+function Equipamento(props) {
+
+    return (
+        <div className="card">
+            <h2>{props.nome}</h2>
+            <p>Setor: {props.setor}</p>
+        </div>
+    )
+}
+
+export default Equipamento;
