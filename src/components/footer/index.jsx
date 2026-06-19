e86cd29ec860804b834dc57dfce6440f1f2eb9cc
Pedro <joaopedropereira2209@gmail.com>
2026-06-18 21:31:02 -0300

    feat: create institutional footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,18 @@
 import "./style.css"
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="footer">
-            Sistema de Gestão para Mineração © 2026
+            <div className="footer-content">
+                <span>© {currentYear} Sistema de Gestão para Mineração</span>
+                <span>
+                    Versão 1.0.0
+                </span>
+            </div>
+            
+            
         </footer>
     );
 }
